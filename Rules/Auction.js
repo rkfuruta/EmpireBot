adc3011f89e90e1669ea21db6226ebe60740d49d
RKFuruta <rcontas@gmail.com>
2023-02-05 00:29:14 -0300

    :bug: Fixed rebid removing item from bid array

--- Rules/Auction.js
+++ Rules/Auction.js
@@ -13,14 +13,11 @@ module.exports = class AuctionUpdate {
         }
         let item = bidItems[update.id];
         let newBidValue = price.getNextBidValue(update.auction_highest_bid);
-        Message.debug(`Checking update on auction ${item.name} Price(${item.raw_value}) Bid(${item.bid_value}) New Bid(${newBidValue})`, "blue");
+        Message.debug(`Checking update on auction ${item.name} Price(${item.raw_value}) Bid(${item.bid_value}) New Bid(${newBidValue})`, "warning");
         item.bid_value = newBidValue;
         if (price.checkAuctionUpdatePrice(item)) {
             Message.debug(`Placing new bid on item ${item.name}\n\tcoins: ${item.value}\n\tbid value: ${item.bid_value}\n\tbuy order from: ${item.buy_order.from}\n\tbuy order: ${item.buy_order.price}`, "warning")
-            let response = await Bid.place(item);
-            if (response) {
-                return update.id;
-            }
+            await Bid.place(item);
         } else {
             return update.id;
         }
